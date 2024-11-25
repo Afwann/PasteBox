26c579592ce0b9115dccd8caf9fc4fc5db4a76a5
Afwan Mulia P <afwanmp@gmail.com>
2024-11-25 17:23:21 +0700

    test: Webhooks

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const connectWithRetry = () => {
     })
     .catch((err) => {
       console.error("Failed to connect to MongoDB", err);
-      setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
+      setTimeout(connectWithRetry, 5000);
     });
 };
 
